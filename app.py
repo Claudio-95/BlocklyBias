7be96b13d42bbf9d2a0abc58db6d2b62727d9b6e
c.demartino <c.demartino1@studenti.unipi.it>
2023-06-02 17:53:54 +0200

    Reordered notebook structure

--- app.py
+++ app.py
@@ -128,87 +128,167 @@ def save_notebook():
     notebook = nbformat.v4.new_notebook()
 
     import_block = []
-    print_block = []
     code_block = []
-
-    previous_line_single_word = False
+    markdown_code = f''
 
     for line in lines:
-        stripped_line = line.strip()
 
-        if stripped_line and len(stripped_line.split()) == 1:
-            if previous_line_single_word:
-                if import_block:
-                    import_cell = nbformat.v4.new_code_cell('\n'.join(import_block))
-                    notebook.cells.append(import_cell)
-                    import_block = []
-
-                if print_block:
-                    print_cell = nbformat.v4.new_code_cell('\n'.join(print_block))
-                    notebook.cells.append(print_cell)
-                    print_block = []
-
-                if code_block:
-                    code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
-                    notebook.cells.append(code_cell)
-                    code_block = []
-
-            code_block.append(line)
-            previous_line_single_word = True
-        elif line.startswith(('import', 'from')):
+        if line.startswith(('import', 'from')):
             # If there are some import or from in current block add them as single import cell
-            if print_block:
-                print_cell = nbformat.v4.new_code_cell('\n'.join(print_block))
-                notebook.cells.append(print_cell)
-                print_block = []
-
             if code_block:
                 code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
                 notebook.cells.append(code_cell)
                 code_block = []
 
             import_block.append(line)
-            previous_line_single_word = False
-        elif line.startswith('    print'):
-            # If the line starts with "print" add it to print block
-            if import_block:
-                import_cell = nbformat.v4.new_code_cell('\n'.join(import_block))
-                notebook.cells.append(import_cell)
-                import_block = []
-
+        elif line.startswith('max_df_edf', 0, (len(line))):
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                code_cell.metadata = {'source_hidden': True}
+                notebook.cells.append(code_cell)
+                code_block = []
+            markdown_code = f'\"EDF (Empirical Differential Fairness) is the ratio between the ratios between positive and total cases of two groups, calculated on the data, without the contribution of a classifier.\"\n\"EDF of the \" + {{biased_cols[0]}} + \" and \" + {{biased_cols[1]}} + \" intersection on the privileged variable \" + {{privileged_cols}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
             if code_block:
                 code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
                 notebook.cells.append(code_cell)
                 code_block = []
-
-            print_block.append(line)
-            previous_line_single_word = False
-        else:
-            # If the line doesn't start with import, from or print add it to code block
+        elif line.startswith('df_metrics', 0, (len(line))):
+            markdown_code = f'\"Equal opportunity is the probability of a privileged individual being classified as such must be the same for everyone. In other words all groups should have similar, or ideally equal, True Positive Rates.\nAlso it is a relaxation of the Equalized Odds, in which it is required that in addition to the same True Positive Rate there is also the same False Positive Rate.\nDemographic Parity is obtained when all groups have the same Predictive Positive Rate.\nThe set of all these metrics are defined here as Fairness metrics.\"\n\"Fairness metrics for \" + {{df_metrics.iloc[0]["class"]}} + \" and \" + {{df_metrics.iloc[1]["class"]}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_disparity', 0, (len(line))):
+            markdown_code = f'\"Disparity is the ratio of its value to the unprivileged group to its value to the privileged group.\"\n\"Disparity on fairness metrics for \" + {{df_metrics.iloc[0]["class"]}} + \" and \" + {{df_metrics.iloc[1]["class"]}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_ratio', 0, (len(line))):
+            markdown_code = f'\"Ratio between positive and negative outcomes:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_mode', 0, (len(line))):
+            markdown_code = f'\"Modal values:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_intersection', 0, (len(line))):
+            markdown_code = f'\"Intersection:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('results_pos', 0, (len(line))):
+            markdown_code = f'\"Positive income mode:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('results_neg', 0, (len(line))):
+            markdown_code = f'\"Negative income mode:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_values_of_1st', 0, (len(line))):
+            markdown_code = f'\"Below is a list that provides the values most frequently associated with\"+ {{max_df_edf.iloc[0, 0]}} + \", and it\'s useful to observe any differences in modal values between the privileged and the unprivileged group:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_values_of_2nd', 0, (len(line))):
+            markdown_code = f'\"Below is a list that provides the values most frequently associated with\"+ {{max_df_edf.iloc[0, 1]}} + \", and it\'s useful to observe any differences in modal values between the privileged and the unprivileged group:\"'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_values_of_outcome_1st', 0, (len(line))):
+            markdown_code = f'\"Instead below there is a list like the previous one but it filters the observations based on the result of the privilege feature, with positive outcome, and it\'s useful to observe the differences between the modal values of the privileged and non-privileged individuals for \" + {{max_df_edf.iloc[0, 0]}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_values_of_outcome_2nd', 0, (len(line))):
+            markdown_code = f'\"Instead below there is a list like the previous one but it filters the observations based on the result of the privilege feature, with positive outcome, and it\'s useful to observe the differences between the modal values of the privileged and non-privileged individuals for \" + {{max_df_edf.iloc[0, 1]}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_values_of_outcome_1st_neg', 0, (len(line))):
+            markdown_code = f'\"Instead below there is a list like the previous one but it filters the observations based on the result of the privilege feature, with negative outcome, and it\'s useful to observe the differences between the modal values of the privileged and non-privileged individuals for \" + {{max_df_edf.iloc[0, 0]}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif line.startswith('df_values_of_outcome_2nd_neg', 0, (len(line))):
+            markdown_code = f'\"Instead below there is a list like the previous one but it filters the observations based on the result of the privilege feature, with negative outcome, and it\'s useful to observe the differences between the modal values of the privileged and non-privileged individuals for \" + {{max_df_edf.iloc[0, 1]}} + \":'
+            markdown_cell = nbformat.v4.new_markdown_cell(markdown_code)
+            notebook.cells.append(markdown_cell)
+            code_block.append(line)
+            if code_block:
+                code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+                notebook.cells.append(code_cell)
+                code_block = []
+        elif len(line.split()) != 1:
             if import_block:
                 import_cell = nbformat.v4.new_code_cell('\n'.join(import_block))
+                import_cell.metadata = {'source_hidden': True}
                 notebook.cells.append(import_cell)
                 import_block = []
-
-            if print_block:
-                print_cell = nbformat.v4.new_code_cell('\n'.join(print_block))
-                notebook.cells.append(print_cell)
-                print_block = []
-
+            # If the line doesn't start with import or from or single words add it to code block
             code_block.append(line)
-            previous_line_single_word = False
 
     # Add the last code block or print line or import block as single cell
     if import_block:
         import_cell = nbformat.v4.new_code_cell('\n'.join(import_block))
         notebook.cells.append(import_cell)
 
-    if print_block:
-        print_cell = nbformat.v4.new_code_cell('\n'.join(print_block))
-        notebook.cells.append(print_cell)
-
     if code_block:
         code_cell = nbformat.v4.new_code_cell('\n'.join(code_block))
+        code_cell.metadata = {'source_hidden': True}
         notebook.cells.append(code_cell)
 
     file_path = os.path.abspath(os.path.dirname(__file__)) + '\static\py\\notebooks\my_notebook.ipynb'
